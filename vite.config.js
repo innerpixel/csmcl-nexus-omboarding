5eebe680c0c2d3f45df31cf0c01bfa4633734ccd
innerpixel <ned.gabrielc@gmail.com>
2025-01-27 00:09:57 +0200

    🎨 Fix favicon and icon references, update base URL configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/csmcl-nexus-omboarding/',
+  base: process.env.NODE_ENV === 'production' ? '/csmcl-nexus-omboarding/' : '/',
   plugins: [
     vue(), 
     VitePWA({
